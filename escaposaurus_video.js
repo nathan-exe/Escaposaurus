37f40d9bfe55406902a27997aa930cf568a3f3ae
nathan-exe <nathan.tazi.1@gmail.com>
2026-09-09 15:45:37 +0200

    des trucs

--- escaposaurus_video.js
+++ escaposaurus_video.js
@@ -102,6 +102,9 @@ function loadGame(folders, files, overlay){
 
     createContactList() ;
     lockContacts() ;
+	//mainHintFound = true ;
+	//unlockContacts() ;
+	//unlockContacts();
 
     /*launch lightbox after creation of the udisk to make it work*/
     startLighBox() ;
